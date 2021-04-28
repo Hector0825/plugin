f826f4567a86069c7f3a7e0496e9be29a3ad20bc
Hector0825 <61753225+Hector0825@users.noreply.github.com>
2021-04-28 18:56:10 +0200

    Exportar a XML

--- plugin.js
+++ plugin.js
@@ -9,9 +9,17 @@ Draw.loadPlugin(function(ui) {
 		
 	    ui.actions.addAction('Descargar', function() {
 	    	var graph = ui.editor.graph;
-			var encoder = new mxCodec();
+			var encoder = new mxCodec(mxUtils.createXmlDocument());
 			var node = encoder.encode(graph.getModel());
+			var str1 = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<mxfile>\n<diagram>\n";
+			// var prueba2 = "<mxfile>\n";
+			// var prueba3 = "<diagram>\n";
+			var str2 = "</diagram>\n</mxfile>";
+			//var prueba5 = "</mxfile>";
+			//var final = prueba + prueba2 + prueba3 + node + prueba4 + prueba5;
 			var xml = mxUtils.getPrettyXml(node);
+			//var final = prueba + prueba2 + prueba3 + node + prueba4 + prueba5;
+			var final = str1 + xml + str2;
 			function download(filename, text) {
 			  var element = document.createElement('a');
 			  element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
@@ -26,22 +34,23 @@ Draw.loadPlugin(function(ui) {
 			}
 
 				// Start file download.
-			download("diagrama.xml",xml);
-			mxUtils.popup(xml, true);
+			download("diagrama.xml", final);
+			mxUtils.popup(final, true);
+
 				//mxUtils.popup(mxUtils.createXmlDocument(node),true);
 	    });
 	    ui.actions.addAction('Visualizar', function(){
 	    	//mxUtils.alert('HOLA')
-	  //   	var req = new mxXmlRequest('https://swapi.dev/api/people/1/', 'GET', false);
-			// req.send();
-			// //mxUtils.popup(mxUtils.getPrettyXml(req.getDocumentElement()), true);
-			// mxUtils.alert(req.getStatus());
+	    	var req = new mxXmlRequest('https://swapi.dev/api/people/1/', 'GET', false);
+			req.send();
+			mxUtils.popup(mxUtils.getlPrettyXml(req.getDocumentElement()), true);
+			mxUtils.alert(req.getStatus());
 			// var sal = req.isBinary();
 
 			// mxUtils.popup(sal, true);
 			var onload = function(req)
 			{
-			if(req.getStatus()==200){
+			if(req.getStatus()=="200"){
 				mxUtils.alert('BIEN');
 			}
 			else{
@@ -58,21 +67,21 @@ Draw.loadPlugin(function(ui) {
 			new mxXmlRequest('https://swapi.dev/api/people/1/', null, 'GET').send(onload, onerror);
 			//mxUtils.alert(req.getStatus());
 
-			var xhr = new XMLHttpRequest(),
-    		method = "GET",
-    		url = "https://swapi.dev/api/people/1/";
+			// var xhr = new XMLHttpRequest(),
+   //  		method = "GET",
+   //  		url = "https://swapi.dev/api/people/1/";
 
-			xhr.open(method, url, true);
-			xhr.onreadystatechange = function () {
-	        //if(xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
+			// xhr.open(method, url, true);
+			// xhr.onreadystatechange = function () {
+	  //       //if(xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
 	            
-	            //document.writeln(xhr.responseText);
-				//console.log(xhr.status);
-				mxUtils.alert(xhr.getStatus);
-	        //}
-		    };
-			xhr.send();
-			mxUtils.alert(xhr.getStatus);
+	  //           //document.writeln(xhr.responseText);
+			// 	//console.log(xhr.status);
+			// 	mxUtils.alert(xhr.getStatus);
+	  //       //}
+		 //    };
+			// xhr.send();
+			// mxUtils.alert(xhr.getStatus);
 		});
 
 	// Adds menu
