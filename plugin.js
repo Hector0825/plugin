e9067ccfe3ab7c0fd71bf8783ec9bd848dc86e37
Hector0825 <61753225+Hector0825@users.noreply.github.com>
2021-05-10 20:40:53 +0200

    CURL FUNCIONANDO

--- plugin.js
+++ plugin.js
@@ -87,21 +87,6 @@ Draw.loadPlugin(function(ui){
 
 
 		ui.actions.addAction('Pruebas', function(){
-function function1() {
-    // stuff you want to happen right away
-    mxUtils.alert('Welcome to My Console,');
-}
-
-function function2() {
-    // all the stuff you want to happen after that pause
-    mxUtils.alert('Blah blah blah blah extra-blah');
-}
-
-// call the first chunk of code right away
-function1();
-
-// call the rest of the code and have it execute after 3 seconds
-setTimeout(function2, 8000);
 
 		});
 
