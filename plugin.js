fd628dbcf17138fc18e220e101256b57be62fa54
Hector0825 <61753225+Hector0825@users.noreply.github.com>
2021-05-12 16:57:56 +0200

    Curl funcionando

--- plugin.js
+++ plugin.js
@@ -1,8 +1,6 @@
 Draw.loadPlugin(function(ui){
 	if (ui.sidebar != null){
 	    mxResources.parse('Descargar=Descargar código Chowlk');
-	    mxResources.parse('Visualizar=Visualizar código');
-	    mxResources.parse('Pruebas=Pruebass');
 
 	    ui.actions.addAction('Descargar', function() {
 	    	var graph = ui.editor.graph;
@@ -31,8 +29,8 @@ Draw.loadPlugin(function(ui){
 				element.click();
 
 				document.body.removeChild(element);
-			}			
-			download("diagrama.xml", final);
+			}	
+		
 			//Llamada a Curl
 			const exec = require('child_process').exec;
 			var descarga = true;
@@ -56,53 +54,13 @@ Draw.loadPlugin(function(ui){
 
 	    });
 
-	    ui.actions.addAction('Visualizar', function(){
-	    	
-			function download(filename, text){
-					  var element = document.createElement('a');
-					  element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
-					  element.setAttribute('download', filename);
-
-					  element.style.display = 'none';
-					  document.body.appendChild(element);
-
-					  element.click();
-
-					  document.body.removeChild(element);
-					}
-
-			const exec = require('child_process').exec;
-			function execute(command, callback) {
-			    var a = exec('curl -F data=@/"diagrama.xml" https://chowlk.linkeddata.es/api', (error, stdout, stderr) => {
-			        callback(stdout);
-			    });
-			};
-			//download("a.owl", a);
-			// call the function
-			execute('ping -c 4 0.0.0.0', (a) => {    
-			        mxUtils.popup(a,true);
-			   		download("a.owl", a);
-			});
-		});
-
-
-		ui.actions.addAction('Pruebas', function(){
-
-		});
-
-
 	// Adds menu
 	    ui.menubar.addMenu('Chowlk', function(menu, parent) {
 	    	ui.menus.addMenuItem(menu, 'Descargar');
-	    	ui.menus.addMenuItem(menu, 'Visualizar');
-	    	ui.menus.addMenuItem(menu, 'Pruebas');
+
 	    });
 	}
 	
 });
 
 
-	//https://chowlk.linkeddata.es/api
-	//install jquery y node
-
-	//curl -F data=@/"diagrama.xml" https://chowlk.linkeddata.es/api
