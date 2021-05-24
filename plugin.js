06d5bb8fbd63d3b55b1278439a1b0f1b8167f476
Hector0825 <61753225+Hector0825@users.noreply.github.com>
2021-05-24 17:53:23 +0200

    Traducido

--- plugin.js
+++ plugin.js
@@ -87,7 +87,7 @@ Draw.loadPlugin(function(ui){
 				var buscar = "shape_id";	
 				var posicion = a.toLowerCase().indexOf(buscar.toLowerCase());
 				if (posicion !== -1)
-				    mxUtils.alert("There are errors in the diagram, incomplete OWL code, use check error options")			 
+				    mxUtils.alert("Error on the diagram, the OWL code is incomplete. Please use the option to check the error")			 
 				});
 	    });	 
 		
